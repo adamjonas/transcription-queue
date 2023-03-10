2f4b4e1984a33cccd27c869453ae52ec2e2bf4cc
nassersaazi <nassersnoop@gmail.com>
2023-03-10 17:12:12 +0300

    Feat: Add swagger documentation set up

--- api/app/routes/user.routes.js
+++ api/app/routes/user.routes.js
@@ -7,6 +7,34 @@ module.exports = app => {
   router.post("/", users.create);
 
   // Retrieve all users
+  /**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder users.
+ *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The user ID.
+ *                         example: 0
+ *                       name:
+ *                         type: string
+ *                         description: The user's name.
+ *                         example: Leanne Graham
+ */
   router.get("/", users.findAll);
 
   // Retrieve a single User with id
